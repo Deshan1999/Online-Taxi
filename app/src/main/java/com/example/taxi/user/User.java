9b2e36eb4755579b0b4cedcf0be01ddfc7bbde3c
Deshan <it20154530@my.sliit.lk>
2021-09-20 15:05:45 +0530

    on

--- app/src/main/java/com/example/taxi/user/User.java
+++ app/src/main/java/com/example/taxi/user/User.java
@@ -1,16 +1,67 @@
 package com.example.taxi.user;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class user {
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String password;
 
-import android.os.Bundle;
+    public user(String id, String name, String email, String phone, String address, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.password = password;
+    }
 
-import com.example.taxi.R;
+    public String getId() {
+        return id;
+    }
 
-public class User extends AppCompatActivity {
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
