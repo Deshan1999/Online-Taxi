acfa50aa583e895f1c567de28bc1efa095cfa015
sugith99 <89339878+sugith99@users.noreply.github.com>
2021-09-23 10:23:48 +0530

    updated v1

--- app/src/main/java/com/example/taxi/driver_management/DriverActivity.java
+++ app/src/main/java/com/example/taxi/driver_management/DriverActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.taxi.R;
+
 public class DriverActivity extends AppCompatActivity {
 
     @Override
