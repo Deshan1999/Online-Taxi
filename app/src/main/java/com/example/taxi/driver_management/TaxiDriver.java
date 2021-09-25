23be9f5c1656163e20bb87067116b1ceceb264ee
Sahan Rasanga <it20106560@my.sliit.lk>
2021-09-26 00:04:46 +0530

    new interface added and java code added

--- app/src/main/java/com/example/taxi/driver_management/TaxiDriver.java
+++ app/src/main/java/com/example/taxi/driver_management/TaxiDriver.java
@@ -1,15 +1,67 @@
 package com.example.taxi.driver_management;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class TaxiDriver {
+    String username;
+    String email;
+    String password;
+    String address;
+    String phoneno;
+    String vehiclenumber;
 
-import android.os.Bundle;
-import com.example.taxi.R;
+    public TaxiDriver(String username, String email, String password, String address, String phoneno, String vehiclenumber) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.phoneno = phoneno;
+        this.vehiclenumber = vehiclenumber;
+    }
 
-public class TaxiDriver extends AppCompatActivity {
+    public String getUsername() {
+        return username;
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_taxi_driver);
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getVehiclenumber() {
+        return vehiclenumber;
+    }
+
+    public void setVehiclenumber(String vehiclenumber) {
+        this.vehiclenumber = vehiclenumber;
     }
 }
--- app/src/main/java/com/example/taxi/driver_management/edit.java
+++ app/src/main/java/com/example/taxi/driver_management/edit.java
@@ -3,7 +3,6 @@ package com.example.taxi.driver_management;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
 import com.example.taxi.R;
 
